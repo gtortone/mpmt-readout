b4803c56f1c298d266ff363c01db17cb3c909c1a
Gennaro Tortone <gennaro.tortone@na.infn.it>
2023-09-25 09:58:34 +0200

    add event receiver utility

---
+++ utils/event-receiver.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import zmq
+from struct import unpack
+from time import sleep
+
+context = zmq.Context()
+
+##
+
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://0.0.0.0:4444")
+topic = "control"
+payload = "start"
+
+sleep(1)
+
+socket.send_string(topic, flags=zmq.SNDMORE)
+socket.send_string(payload)
+
+##
+
+frontend = context.socket(zmq.ROUTER)
+
+poller = zmq.Poller()
+poller.register(frontend, zmq.POLLIN)
+frontend.bind("tcp://*:5555")
+
+while True:
+    message = frontend.recv_multipart()
+    print("message received")
+    #print(message[1][0:32])
